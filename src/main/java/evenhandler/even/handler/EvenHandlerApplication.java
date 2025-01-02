fb5c2dbcd7f5367f231e3e2ce21016f7b2c0cae5
Naduladisanayaka494 <disanayakanadula494@gmail.com>
2025-01-02 13:37:09 +0530

    project folder structure created

---
+++ src/main/java/evenhandler/even/handler/EvenHandlerApplication.java
@@ -0,0 +1,13 @@
+package evenhandler.even.handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/evenhandler/even/handler/EvenHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package evenhandler.even.handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
